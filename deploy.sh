e0145aaabc59378fe6763172dfad961103da39f5
源源源 <2632395964@qq.com>
2020-06-05 21:45:16 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,17 +4,17 @@
 set -e
 
 # 生成静态文件
-npm run build
+npm run docs:build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
-# echo 'www.yourwebsite.com' > CNAME
+# echo 'www.example.com' > CNAME
 
 git init
-git add .
-git commit -m "add files"
+git add -A
+git commit -m 'deploy'
 
 git push -u origin master
 
