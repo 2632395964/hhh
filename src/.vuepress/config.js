60324c0adc30d7f2be6bf64a43cd5c77cbe7fad6
源源源 <2632395964@qq.com>
2020-06-05 20:32:45 +0800

    add files

---
+++ src/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    title:'源的博客',
+    head:[
+        ['link', {rel:'icon',herf:'#'}],
+    ],
+    themeConfig:{
+        logo:'#',
+        nav:[
+        {text:'首页',link:'/'},
+        {text:'技术文档',link: '/tech/'},
+        {text:'简书主页',link: 'http://www.hao123.com'}],
+    sidebar:'auto',
+    }
+   
+}; 
